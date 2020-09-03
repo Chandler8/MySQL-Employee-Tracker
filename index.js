16938818829e9003d19aa0cd50356302aeab3a89
Chandler L <cbl0015@auburn.edu>
2020-09-03 18:14:06 -0400

    Incorportating MySQL database info

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Auburn",
-    database: ""
+    database: "employees_DB"
   });
   
   // Initialize connection 
